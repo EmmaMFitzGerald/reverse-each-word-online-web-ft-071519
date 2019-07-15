19c4126c80975f29d52dd89e7e666bdf82bf4139
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-15 18:46:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
   reversed_string = ""
   i = 0 
-  while i < string.length 
-  reversed_string = string[i] + reversed_string
+  while i < sentence.length 
+  reversed_string = sentence[i] + reversed_string
   i += 1 
   end
  return reversed_string
