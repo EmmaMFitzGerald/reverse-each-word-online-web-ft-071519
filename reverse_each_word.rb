fdd6c83bca30a15dc954076f033c1e1f3d503a2c
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-15 18:46:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,9 @@
 def reverse_each_word(sentence)
-  new_array = sentence.split("")
-  new_array.each do |word|
-    word.reverse
-  end
-  new_array.join("")
-end
-
- reversed_string = ''
- 
- i = 0 
- while i < string.length 
+  reversed_string = ""
+  i = 0 
+  while i < string.length 
   reversed_string = string[i] + reversed_string
   i += 1 
- end
- 
+  end
  return reversed_string
 end
