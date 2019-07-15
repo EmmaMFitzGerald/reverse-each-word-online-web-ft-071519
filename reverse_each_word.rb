b770ff91a11577475cfa3ac61ca5056a0ec7edd3
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-15 18:57:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,6 @@
 require "pry"
 
 def reverse_each_word(sentence)
-  new_array = sentence.split(" ")
-  reversed_array = new_array.each do |word|
-    word.reverse!
-  end
-  return reversed_array.join(" ")
-end
-
-def reverse_each_word(sentence)
   sentence.split(" ").collect do |word|
     binding.pry
     word.reverse!.join(" ")
